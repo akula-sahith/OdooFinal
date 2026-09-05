bf730e1113835861e7694acb7437a412d441626b
Sahith Akula <akulasahith268@gmail.com>
2026-09-05 13:01:32 +0530

    SpringBoot Project is Initialized

---
+++ DealFlow360_backend/src/test/java/com/odoo/DealFlow360/DealFlow360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odoo.DealFlow360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealFlow360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
